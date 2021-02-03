1ea45c2ece279b28992c69ce41c3ccee7f2024c1
MikeDudolevitch <mikedudolevitch@gmail.com>
2021-02-03 17:26:40 -0500

    commit tree 0b3ed3d2e37e8fe761a482bfb602bf29e83e0ad6

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,43 @@
-
+def sort_array_asc(array)
+    array.sort do |a, b|
+        a <=> b
+    end
+end
+def sort_array_desc(array)
+    array.sort {|a, b| b <=> a}
+end
+def sort_array_char_count(array)
+    array.sort {|a, b| a.length <=> b.length}
+end
+def reverse_array(array)
+    array.reverse
+end
+def swap_elements(array)
+    array[1], array[2] = array[2], array[1]
+    array
+end
+def kesha_maker(array)
+    array.each do |i|
+        i[2] = "$"
+    end
+end
+def find_a(array)
+    array.find_all do |a|
+        a[0] == "a"
+    end
+end
+def sum_array(array)
+    sum = 0
+        array.each do |n|
+            sum = sum + n
+        end
+        sum
+end
+def add_s(array)
+    array.collect do |word|
+        if array[1] == word
+            word
+        else word+"s"
+end
+end
+end
